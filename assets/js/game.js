11e28ae6b60c1ee2c658304f5511be4208f40c15
Jmax <jmax407@gmail.com>
2020-12-31 21:31:27 -0500

    variables and console.log

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,8 @@
-window.alert("This is an alert! JavaScript is running!");
+
+var playerName = window.prompt("what is your robot's name?");
+function fight() {
+    window.alert("The fight has begun!");
+}
+
+// fight();
+console.log(playerName);
